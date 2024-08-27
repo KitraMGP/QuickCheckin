2f8d61b9f7cc60bda538ed38a34d5ebcb37e564a
KitraMGP <zijing233@gmail.com>
2024-08-27 23:24:32 +0800

    update Course and Student class

--- app/src/main/java/kitra/quickcheckin/business/common/Course.java
+++ app/src/main/java/kitra/quickcheckin/business/common/Course.java
@@ -1,5 +1,60 @@
 package kitra.quickcheckin.business.common;
 
-public class Course {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class Course {
+    private String name;
+    private String id;
+    private Set<Student> studentList;
+
+    public Course(String name, String id) {
+        this.name = name;
+        this.id = id;
+        this.studentList = new HashSet<>();
+    }
+
+    public Course(String name, String id, Set<Student> studentList) {
+        this.name = name;
+        this.id = id;
+        this.studentList = studentList;
+    }
+
+    public int getStudentCount() {
+        return studentList.size();
+    }
+
+    public boolean hasStudent(String id) {
+        return studentList.stream().anyMatch(student -> student.getId().equals(id));
+    }
+
+    public boolean addStudent(Student s) {
+        return studentList.add(s);
+    }
+
+    public boolean removeStudent(String id) {
+        return studentList.removeIf(student -> student.getId().equals(id));
+    }
+
+    public List<Student> getStudents() {
+        return new ArrayList<>(this.studentList);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
